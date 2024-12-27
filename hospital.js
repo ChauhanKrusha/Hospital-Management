18ebdefaf31b2a6c66707a51b225996df3c0fbed
ChauhanKrusha <chauhankrusha2005@gmail.com>
2024-12-27 13:27:06 +0530

    Add files via upload

---
+++ hospital.js
@@ -0,0 +1,4 @@
+let menubar=document.querySelector('menubar');
+let mynav=document.querySelector('.navbar');
+menubar.onclick=()=>{menubar.classList.toggle('fa-times')}
+mynav.onclick=()=>{menubar.classList.toggle('active')}
